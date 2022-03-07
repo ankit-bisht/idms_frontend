9b8715160fa2b0ca20d726a86bbdc904d90735a7
suruchikaursihra <suruchikaursihra@gmail.com>
2022-03-07 20:39:29 +0530

    push build

--- src/app/configuration/config.ts
+++ src/app/configuration/config.ts
@@ -3,7 +3,7 @@ const LOCAL_SERVER = 'http://localhost:4400/api/v1/';
 
 export class Configuration {
 
-  public baseUrl = LOCAL_SERVER;
+  public baseUrl = PROD_SERVER;
   public Hmac256Secret = 'abcdefg';
   public UrlObj = {
     loginApi: this.baseUrl + "userLogin",
